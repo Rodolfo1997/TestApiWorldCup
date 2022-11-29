42ac063e55de05e6b5c7d0ff9f0594469cfef9d6
Rodolfo <rodolfostopinto97@gmail.com>
2022-11-29 19:59:07 -0300

    initial commit

---
+++ TestApiWorldCup/Program.cs
@@ -0,0 +1,82 @@
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestApiWorldCup
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string urlApiContagem = "https://localhost:44335/api/v1/teams/team/";
+            var client = new HttpClient();
+            var graphQLClient = new GraphQLHttpClient("https://localhost:44336/graphql/../api", new NewtonsoftJsonSerializer());
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                var random = new Random();
+                var response = await client.GetAsync($"{urlApiContagem}{random.Next(1, 3)}");
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine("REST API");
+                    Console.WriteLine("");
+
+                    var conteudo = await response.Content.ReadAsStringAsync();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Get realizado com sucesso. Status Code= {response.StatusCode} Message - {conteudo}");
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("GraphQL");
+                Console.WriteLine("");
+
+                var request = new GraphQLRequest
+                {
+                    Query = @"
+                    {
+                      findTeamBy(key: 1){
+                        name,
+                        bestPlayer{
+                          name,
+                          team
+                        }
+                      }
+                    }",
+
+                    Variables = new
+                    {
+                        key = random.Next(1, 3)
+                    }
+                };
+
+                var graphQLResponse = await graphQLClient.SendQueryAsync<object>(request);
+                Console.WriteLine(graphQLResponse.Data.ToString());
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(10000);
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestApiWorldCup/TeamDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApiWorldCup
+{
+    public class TeamDTO
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+        public int Titles { get; set; }
+        public string Shield { get; set; }
+        public PlayerDTO BestPlayer { get; set; }
+        public List<PlayerDTO> Players { get; set; }
+    }
+}
